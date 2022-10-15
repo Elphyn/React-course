9c54f3b34663d2f757416af56224fd51f3853e27
Vlad <elphynkarelov@gmail.com>
2022-10-15 13:47:24 +0900

    almost fineshed jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,14 @@ function App() {
   return (
     <Card>
       <img src="/src/assets/Girl.png" alt="" />
-      <Button/>
+      <Info/>
+      <div className='btn-sect'>
+        <Button/>
+        <Button/>
+      </div>
+      <About/>
+      <Interests/>
+      
     </Card>
   )
 }
@@ -20,9 +27,37 @@ function Card(props){
   )
 }
 
+function Info(){
+  return(
+    <div className='Info'>
+      <h1>Laura Smith</h1>
+      <h3>Frontend Developer</h3>
+      <h4>laurasmith.website</h4>
+    </div>
+  )
+}
+
 function Button(props){
   return(
     <a href="https://www.google.com/" className='Button'>Button</a>
+  )
+}
+
+function About(){
+  return(
+    <div>
+      <h1>About</h1>
+      <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+    </div>
+  )
+}
+
+function Interests(){
+  return(
+    <div className='flex'>
+      <h1>Interests</h1>
+      <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+    </div>
   )
 }
 
